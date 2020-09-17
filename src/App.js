ea48a1a270a5abdabb9ad06e9762af057f615137
Shubham Chitransh <shubhamchitransh8@gmail.com>
2020-09-17 23:50:12 +0530

    created landing page

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
-import React from "react";
+import React, { useEffect } from "react";
 //?components
 import Header from "./components/header";
 import Body from "./components/body";
 import Footer from "./components/footer";
 //?styles
 import styles from "./App.module.css";
+import { render } from "@testing-library/react";
 
-function App() {
-   return (
-      <div className={styles.App}>
-         <Header />
-         <Body />
-         <Footer />
-      </div>
-   );
+class App extends React.Component {
+   onUnload = (e) => {
+      e.preventDefault();
+      e.returnValue = "Are you sure you want to reload?";
+   };
+
+   componentDidMount() {
+      window.addEventListener("beforeunload", this.onUnload);
+   }
+
+   componentWillUnmount() {
+      window.removeEventListener("beforeunload", this.onUnload);
+   }
+
+   render() {
+      return (
+         <div className={styles.App}>
+            <Header />
+            <Body />
+            <Footer />
+         </div>
+      );
+   }
 }
 
 export default App;
